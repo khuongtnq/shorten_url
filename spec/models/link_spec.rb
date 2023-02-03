340da6ab5bf9563af2e5eb837d2e9767202320a4
Khuong Truong <khuong.truong@deliveree.com>
2023-02-03 09:13:14 +0700

    write many unit test

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Link, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect do
+      create(:link)
+    end.to change(Link, :count).by(1)
+  end
+
+  it "is not valid wrong long_url format" do
+    link = Link.new(long_url: "abcxyz.com")
+    expect(link).to_not be_valid
+  end
 end
